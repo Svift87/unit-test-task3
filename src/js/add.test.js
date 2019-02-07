1eac74059118c3e99cff55f361bf2c410981c121
Sergey <kuryakov-s@mail.ru>
2019-02-07 23:09:04 +0700

    fix #1

---
+++ src/js/add.test.js
@@ -0,0 +1,30 @@
+import fetchData from './http';
+import getLevel from './add';
+
+jest.mock('./http');
+
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
+test('testung return value for response.status !==\'ok\'', () => {
+  const response = {
+    status: 'notOk',
+    level: -42,
+  };
+  fetchData.mockReturnValue(response);
+
+  const level = getLevel(1);
+  expect(level).toBe('Информация об уровне временно недоступна');
+});
+
+test('testing level value for ok response', () => {
+  const response = {
+    status: 'ok',
+    level: 42,
+  };
+  fetchData.mockReturnValue(response);
+  const level = getLevel(1);
+  const expected = 'Ваш текущий уровень: 42';
+  expect(level).toBe(expected);
+});
